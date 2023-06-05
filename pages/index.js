ecf420300e0f945124103142b7f0457188b8346e
Naufaldwar <naufaldwiariananto@gmail.com>
2023-06-06 01:05:55 +0700

    ada discound and price

--- pages/index.js
+++ pages/index.js
@@ -7,16 +7,16 @@ import variant2 from "../assets/varian2.png";
 import variant3 from "../assets/variant3.png";
 import variant4 from "../assets/variant4.png";
 import variant5 from "../assets/variant5.png";
-import variant6 from "../assets/variant6.jpg";
-import variant7 from "../assets/variant7.jpg";
-import variant8 from "../assets/variant8.jpg";
-import variant9 from "../assets/variant9.jpg";
-import variant10 from "../assets/variant10.jpg";
-import variant11 from "../assets/11.jpg";
-import variant12 from "../assets/12.jpg";
-import variant13 from "../assets/13.jpg";
-import variant14 from "../assets/14.jpg";
-import variant15 from "../assets/15.jpg";
+import variant6 from "../assets/variant6.png";
+import variant7 from "../assets/variant7.png";
+import variant8 from "../assets/variant8.png";
+import variant9 from "../assets/variant9.png";
+import variant10 from "../assets/variant10.png";
+import variant11 from "../assets/11.png";
+import variant12 from "../assets/12.png";
+import variant13 from "../assets/13.png";
+import variant14 from "../assets/14.png";
+import variant15 from "../assets/15.png";
 import ukuran1 from "../assets/ukuran.jpg";
 import ukuran2 from "../assets/ukuran2.jpg";
 import ukuran3 from "../assets/ukuran3.jpg";
@@ -27,6 +27,7 @@ import { Button, Container, Divider, Flex, Text } from "@mantine/core";
 import Image from "next/image";
 import { useState } from "react";
 import { data } from "autoprefixer";
+import { IconBookmarkFilled } from "@tabler/icons-react";
 
 // const inter = Inter({ subsets: ["latin"] });
 
@@ -36,7 +37,9 @@ export default function Home() {
     name: "Shadira Dress",
     description:
       "Shadira dress hadir dengan desain dress 2 in 1 yang terdiri dari dress dan vest motif tartan (kotak-kotak). Gamis dalam terbuat dari bahan cey airflow dan vest bagian luar berbahan royal tartan. Bahan cey airflow merupakan bahan yang memiliki karakteristik tekstur crinkle, bahan yang jatuh, ironless, stretch, dan halus.Sedangkan vest bermotif kotak-kotak menggunakan bahan royal tartan yang lembut, jatuh, adem.",
-    price: "Rp. 250.000",
+    pricetrought: "Rp. 350.000",
+    discount: "16%",
+    price: "Rp. 294.000",
     image: shadira,
     variant: [
       {
@@ -73,7 +76,9 @@ export default function Home() {
     name: "Ziya Simple Dress",
     description:
       "Ziya Dress menggunakan bahan Shakila, memiliki karakter adem, lembut, ringan, stretchy, jatuh, serat diagonal sehingga nyaman untuk aktifitas. Looknya cantik tapi tetap sederhana dan nyaman digunakan kemanapun",
-    price: "Rp. 250.000",
+    pricetrought: "Rp. 340.000",
+    discount: "12%",
+    price: "Rp. 299.200",
     image: ziya,
     variant: [
       {
@@ -110,7 +115,9 @@ export default function Home() {
     name: "Alula 3in1 Dress Korean dress with vest",
     description:
       "Alula 3in1 Dress Korean dress with vest menggunakan bahan Shakila, memiliki karakter adem, lembut, ringan, stretchy, jatuh, serat diagonal sehingga nyaman untuk aktifitas. Looknya cantik tapi tetap sederhana dan nyaman digunakan kemanapun",
-    price: "Rp. 250.000",
+    price: "Rp. 297.500",
+    pricetrought: "Rp. 350.000",
+    discount: "15%",
     image: alula,
     variant: [
       {
@@ -170,7 +177,7 @@ export default function Home() {
           m={"md"}
         >
           <Image src={logo} alt="logo" className="w-60 md:w-80" />
-          <Text size="24px">Fashion Muslimah Trendi</Text>
+          <Text size="24px">Fashion Muslimah Trenndi</Text>
           <Image src={hero} alt="hero" className="w-[700px]" />
         </Flex>
         <Divider my={"md"} />
@@ -186,13 +193,29 @@ export default function Home() {
           // justify={"space-evenly"}
           className="flex flex-col md:flex-row justify-center items-center md:justify-evenly"
         >
-          <Image
-            src={dataProduk.image}
-            alt="shadira"
-            className="md:h-full w-40"
-            // width={400}
-            // height={100}
-          />
+          <div className="relative">
+            <Flex gap={"md"} align={"start"}>
+              <p className="font-semibold">{dataProduk.price}</p>
+              <p className="line-through text-red-400">
+                {dataProduk.pricetrought}
+              </p>
+              <IconBookmarkFilled
+                size={90}
+                textAnchor="asdas"
+                className="text-red-600"
+              />
+              <p className="absolute right-7 top-7 text-white font-semibold">
+                -{dataProduk.discount}
+              </p>
+            </Flex>
+            <Image
+              src={dataProduk.image}
+              alt="shadira"
+              className="md:h-full w-40"
+              // width={400}
+              // height={100}
+            />
+          </div>
           <Flex
             className="md:w-[500px] w-80 text-center md:text-justify"
             direction={"column"}
@@ -220,7 +243,12 @@ export default function Home() {
                 align={"center"}
                 key={item.id}
               >
-                <Image src={item.image} alt="shadira" height={300} />
+                <Image
+                  src={item.image}
+                  alt="shadira"
+                  height={300}
+                  className="hover:scale-110"
+                />
                 <Text>Variant {item.name}</Text>
               </Flex>
             </>
@@ -248,7 +276,7 @@ export default function Home() {
         {/* Produk Lainnya */}
         <Flex justify={"center"} mt={"lg"}>
           <Text size="24px" fw={"bolder"} align="center">
-            Produk Lainnya
+            Best Seller
           </Text>
         </Flex>
         <Flex
@@ -260,8 +288,15 @@ export default function Home() {
         >
           <Flex
             onClick={handleClick1}
-            className="border w-80 h-80 flex-col rounded-xl p-3 justify-center items-center gap-2 hover:cursor-pointer hover:scale-105"
+            className="relative shadow-lg border w-80 h-80 flex-col rounded-xl p-3 justify-center items-center gap-2 hover:cursor-pointer hover:scale-105"
           >
+            <IconBookmarkFilled
+              size={90}
+              className="text-red-600 absolute top-0 right-0"
+            />
+            <p className="absolute right-7 top-7 text-white font-semibold">
+              -{data2.discount}
+            </p>
             <Image
               src={data2.image}
               alt="shadira"
@@ -273,8 +308,15 @@ export default function Home() {
           </Flex>
           <Flex
             onClick={handleClick2}
-            className="border w-80 h-80 flex-col rounded-xl p-3 justify-center items-center gap-2 hover:cursor-pointer hover:scale-105"
+            className="relative shadow-lg border w-80 h-80 flex-col rounded-xl p-3 justify-center items-center gap-2 hover:cursor-pointer hover:scale-105"
           >
+            <IconBookmarkFilled
+              size={90}
+              className="text-red-600 absolute top-0 right-0"
+            />
+            <p className="absolute right-7 top-7 text-white font-semibold">
+              -{data1.discount}
+            </p>
             <Image
               src={data1.image}
               alt="shadira"
@@ -286,8 +328,15 @@ export default function Home() {
           </Flex>
           <Flex
             onClick={handleClick3}
-            className="border w-80 h-80 flex-col rounded-xl p-3 justify-center items-center gap-2 hover:cursor-pointer hover:scale-105"
+            className="relative shadow-lg border w-80 h-80 flex-col rounded-xl p-3 justify-center items-center gap-2 hover:cursor-pointer hover:scale-105"
           >
+            <IconBookmarkFilled
+              size={90}
+              className="text-red-600 absolute top-0 right-0"
+            />
+            <p className="absolute right-7 top-7 text-white font-semibold">
+              -{data3.discount}
+            </p>
             <Image
               src={data3.image}
               alt="shadira"
